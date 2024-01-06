4c8452b0260d7a153b5659be07ea2dbb340f7f9b
starringRashmi <purkayastharashmi@gmail.com>
2024-01-06 17:50:52 +0530

    first-sec completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,44 +1,41 @@
-import React from 'react'
+import React from "react";
 import logo from "../assets/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 
-
-
-
 const Navbar = () => {
   return (
-    <div className="w-[1550px] h-[60px] flex  items-center  bg-[#ffffff] overflow-x-hidden overflow-y-hidden fixed z-50">
-      <div className="flex">
+    <div className="w-[100vw] h-[60px] md:flex flex-row hidden justify-around  mx-auto items-center bg-[#ffffff]  fixed z-50 ">
+      <div>
         <Link to="/">
           <img
-            className="absolute text-black  w-[117px] h-[69px] top-[0px] md:top-[-5px] left-[0px] md:left-[100px]"
+            className="   "
             src={logo}
             alt="Logo"
             loading="lazy"
           ></img>
         </Link>
-        <div>
-          <ul className=" md:flex items-center gap-x-10 absolute hidden md:top-[11px] md:left-[816px]">
-            <li className=" hover:text-deepgreen hover:underline">
-              <Link to="/">Home</Link>
-            </li>
-            <li className=" hover:text-deepgreen hover:underline">
-              <Link to="/">Join as partner</Link>
-            </li>
-            <li className=" hover:text-deepgreen hover:underline">
-              <Link to="/">Blogs</Link>
-            </li>
-            <li className=" hover:text-deepgreen hover:underline">
-              <Link to="/">About us</Link>
-            </li>
-                      <button className=' w-[137px] h-[42px] border rounded-xl  px-[18px] text-white  bg-[#EAB308]'>
-                        Partner Login
-                      </button>        
-          </ul>
-        </div>
+      </div>
+      <div className="   ">
+        <ul className=" flex space-x-10  items-center">
+          <li className=" hover:text-deepgreen hover:underline">
+            <Link to="/">Home</Link>
+          </li>
+          <li className=" hover:text-deepgreen hover:underline">
+            <Link to="/">Join as partner</Link>
+          </li>
+          <li className=" hover:text-deepgreen hover:underline">
+            <Link to="/">Blogs</Link>
+          </li>
+          <li className=" hover:text-deepgreen hover:underline">
+            <Link to="/">About us</Link>
+          </li>
+          <button className=" w-[137px] h-[42px] border rounded-xl  px-[18px] text-white  bg-[#EAB308]">
+            Partner Login
+          </button>
+        </ul>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,42 +1,42 @@
-import React from 'react'
-import arrow from "../assets/arrow.png"
-import ellipse from "../assets/Ellipse 422.png"
+import React from "react";
+import arrow from "../assets/arrow.png";
+import ellipse from "../assets/Ellipse 422.png";
 import ellipse2 from "../assets/ellipse1.png";
 import ellipse_sm from "../assets/ellip-sm.png";
-import ellipse_sm1 from "../assets/ellipse-sm2.png"
+import ellipse_sm1 from "../assets/ellipse-sm2.png";
 import ellipse_sm2 from "../assets/ellipse-sm3.png";
 import ellipse_sm3 from "../assets/ellipse-sm4.png";
 
 const Homepage = () => {
   return (
-    <div className="w-full h-[707px] overflow-x-hidden overflow-y-hidden  bg-slate-50">
-      <div className="flex  relative items-center">
+    <div className="relative w-full h-[49.5vw] overflow-x-hidden overflow-y-hidden  bg-slate-50">
+      <div className="flex  items-center">
         {/* /left part */}
-        <div className="flex flex-col absolute w-[636px] h-[488px] top-[197px] left-[83px] gap-9">
-          <div className="w-[217px] h-[46px] border rounded-md py-[4px] px-[16px] gap-2.5 bg-amber-50">
-            <div className="w-[190px] h-[38px] font-roboto text-lg leading-9 font-semibold items-center text-[#EAB308] z-50">
+        <div className="flex flex-col absolute w-[53rem] h-[35rem] top-[12vw] left-[4.5vw] gap-9">
+          <div className="w-[14rem] h-[3rem] border rounded-md py-[0.25rem] px-[1rem] gap-2.5 bg-amber-50">
+            <div className="w-[12rem] h-[2.3rem] font-roboto text-lg leading-9 font-semibold items-center text-[#EAB308] z-50">
               Welcome to SnackBae
             </div>
           </div>
-          <div className="w-[636px] h-[242px]  flex flex-col z-40">
-            <div className="w-[636px] h-[180px] font-roboto font-semibold text-5xl leading-none text-slate-950 -tracking-[2%]">
+          <div className="w-[39.5rem] h-[14rem]  flex flex-col z-40">
+            <div className="w-[39.5rem] h-[12rem] font-roboto font-semibold text-5xl leading-none text-slate-950 -tracking-[2%]">
               Discover, Review, Earn Coupons, and Connect with our vibrant food
               community.
             </div>
-            <div className="w-[550px] h-[50px] font-opensans font-normal text-base leading-6 -tracking-[2%] text-slate-950 mt-5 mb-4">
+            <div className="w-[35rem] h-[4rem] font-opensans font-normal text-base leading-6 -tracking-[2%] text-slate-950 mt-5 mb-4">
               Lorem ipsum dolor sit amet, consectetur adipiscing e Lorem ipsum
               dolordolor sit ametamet, consecteturconsectetur adipiscing erem
               ipsum dolor sit a...
             </div>
-            <div className=" flex flex-row  w-[376px] h-[42px] space-x-6  items-center mb-6">
-              <button className=" flex py-3 px-3 w-[158px] relative  h-[42px] border rounded-lg space-x-2 items-start justify-start bg-[#EAB308]">
+            <div className=" flex flex-row  w-[22rem] h-[3.5rem] space-x-6  items-center mb-3">
+              <button className=" flex py-3 px-3 w-[10rem] relative  h-[2.8rem] border rounded-lg space-x-2 items-start justify-start bg-[#EAB308]">
                 <img
-                  className="absolute w-[18px] h-[18px]"
+                  className="absolute w-[1.2rem] h-[1.2rem]"
                   src={arrow}
                   alt="Logo"
                   loading="lazy"
                 ></img>
-                <p className="font-Roboto font-medium w-[121px] h-[11px] text-white text-base leading-4 ">
+                <p className="font-Roboto font-medium w-[9rem] h-[0.75rem] text-white text-base leading-4 ">
                   Download App
                 </p>
               </button>
@@ -46,8 +46,8 @@ const Homepage = () => {
                 </p>
               </button>
             </div>
-            <div className="flex flex-row font-opensans w-[339px] gap-3 items-center justify-center ">
-              <div className="flex relative w-[152px]">
+            <div className="flex flex-row font-opensans w-[21rem] gap-3 items-center justify-center ">
+              <div className="flex relative w-[10rem]">
                 <div className="flex my-auto w-[79%]">
                   <div className="flex my-auto w-[74%]">
                     <img
@@ -56,21 +56,21 @@ const Homepage = () => {
                     ></img>
                     <img
                       src={ellipse_sm1}
-                      className=" w-14 h-14 ml-[-24px] my-auto rounded-[50%] z-[1]border-2 border-[#ffffff]"
+                      className=" w-14 h-14 ml-[-25%] my-auto rounded-[50%] z-[1] border-2 border-[#ffffff]"
                     ></img>
                   </div>
                   <img
                     src={ellipse_sm2}
-                    className=" w-14 h-14 ml-[-24px] my-auto rounded-[50%] z-[1]border-2 border-[#ffffff]"
+                    className=" w-14 h-14 ml-[-23%] my-auto rounded-[50%] z-[1]  border-2 border-[#ffffff]"
                   ></img>
                 </div>
                 <img
                   src={ellipse_sm3}
-                  className=" w-14 h-14 ml-[-24px] rounded-[50%] my-auto z-[1]border-2 border-[#ffffff]"
+                  className=" w-14 h-14 ml-[-18%] rounded-[50%] my-auto z-[1] border-2 border-[#ffffff]"
                 ></img>
               </div>
               <div className="flex flex-col items-start justify-start w-auto">
-                <p className="text0left sm:text-3xl md:text[32px] text-[34px] text-black-900 w-auto font-bold font-opensans">
+                <p className="text-left sm:text-3xl md:text[2rem] text-[3rem] text-black-900 w-auto font-bold font-opensans">
                   1000+
                 </p>
                 <div className="flex flex-col items-start justify-start w-auto">
@@ -83,15 +83,15 @@ const Homepage = () => {
           </div>
         </div>
         {/* right part */}
-        <div className=" flex justify-center ">
+        <div className=" flex  justify-around my-auto mx-auto ">
           <img
-            className="relative w-[930px] h-[639px] top-[130px] items-center justify-center left-[616px] "
+            className=" absolute w-[60vw] h-[110vh] top-[7vw]  right-[-3vw] "
             src={ellipse}
             alt="Logo"
             loading="lazy"
           ></img>
           <img
-            className="absolute w-[850px] h-[600px] top-[168px] left-[678px]  z-20"
+            className="absolute w-[70vw] h-[99vh] top-[6.4vw] right-[-9vw] object-scale-down z-20 place-content-around"
             src={ellipse2}
             alt="Logo"
             loading="lazy"
@@ -100,6 +100,6 @@ const Homepage = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Homepage
+export default Homepage;
