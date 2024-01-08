43fe93791af71716469052a1379e4e013ff2a1e3
Vishal Kumar <127188277+vikrantvishal@users.noreply.github.com>
2024-01-08 23:17:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar';
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Homepage";
 import Joinpartner from './Pages/Joinpartner';
+import AboutUs from "./Pages/AboutUs";
+
 function App() {
   return (
    <div className='app'>
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/joinpartner" element={<Joinpartner/>}></Route>
+        <Route path="/aboutUs" element={<AboutUs />}></Route>
       </Routes>
    </div>
   );
