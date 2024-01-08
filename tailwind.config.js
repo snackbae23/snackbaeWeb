35c778d01f42e09f375d20f8026cc2fa36f1655e
Vishal Kumar <127188277+vikrantvishal@users.noreply.github.com>
2024-01-08 23:20:31 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'opensans': ['ui-sans-serif', 'system-ui', 'sans-serif', '\"Open Sans\"', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      }
     },
 
   },
