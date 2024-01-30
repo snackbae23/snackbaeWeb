48778c321dbe31d34aba7e7e9bab80fd62ed63c5
Nitish0604 <iamnitishsah@gmail.com>
2024-01-31 02:53:20 +0530

    Payout

--- src/Pages/Payout.js
+++ src/Pages/Payout.js
@@ -60,27 +60,71 @@ const Payout = () => {
             <div className='flex  w-full mt-20 h-full '>
                 {/* left */}
                 <div className='w-[20%]  bg-white  '>
-                    <Link to='/' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-blue hover:text-white gap-2 ' >
+                    <Link to='/' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-yellow-600 hover:text-white gap-2 ' >
                         <FaHome /> <p>Dashboard</p>
                     </Link>
-                    <Link to='/blogs' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-blue hover:text-white gap-2' >
+                    <Link to='/blogs' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-yellow-600 hover:text-white gap-2' >
                         <FaBlog /><p>Payout</p>
                     </Link>
-                    <Link to='appointment' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-blue hover:text-white gap-2'>
+                    <Link to='appointment' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-yellow-600 hover:text-white gap-2'>
                         <GiBilledCap /><p>Menu</p>
                     </Link>
-                    <Link to='/cLink' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-blue hover:text-white gap-2'>
+                    <Link to='/cLink' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-yellow-600 hover:text-white gap-2'>
                         <FaPhoneAlt /><p>Edit store detail</p>
                     </Link>
-                    <Link to='/' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-blue hover:text-white gap-2'>
+                    <Link to='/' className='w-full h-[50px] bg-slate-100 mb-1 flex items-center p-4 hover:bg-yellow-600 hover:text-white gap-2'>
                         <IoIosLogOut /> <p>Log out</p>
                     </Link>
                 </div>
                 {/* right */}
-                <div className='w-[75%] bg-slate-300 ml-6 h-full rounded-md' >
-                   <div className=''>
+                <div className='w-[75%] bg-slate-300 ml-6 h-full rounded-md p-6' >
+                    <div className='w-full h-[100px] flex items-center '>
+                        <h1 className='text-[1rem] font-bold w-[40%] ml-4 '>Payout</h1>
+                        <div className='w-[60%] flex  gap-4 justify-end'>
+                            <button className='bg-black text-white w-[90px] h-10 rounded-md'>weekly</button>
+                            <button className='bg-white  w-[90px] h-10 rounded-md'>Monthly</button>
+                            <button className='bg-white  w-[90px] h-10 rounded-md'>Yearly</button>
+                            <input className='rounded-md'
+                                type="date">
+                            </input>
+                        </div>
 
-                   </div>
+
+                    </div>
+
+                    <div className='w-full h-[190px] bg-white rounded-md flex'>
+                        <div className='w-[25%] flex flex-col  items-center justify-center '>
+                            <p>Available for payout</p>
+                            <p className=' font-bold text-[2.1rem]'>$1,712.00</p>
+                            <button className='w-[200px] bg-yellow-500 h-10 rounded-md'>Withdraw Request</button>
+                             
+                        </div>
+                        <div className='h-[70%] w-[2px] bg-slate-200 mt-5'></div>
+                        <div className='w-[25%] flex flex-col  items-center justify-center '>
+                            <p>Payment being cleared</p>
+                            <p className=' font-bold text-[2.1rem]'>$112.00</p>
+                            
+
+                        </div>
+                        <div className='h-[70%] w-[2px] bg-slate-200 mt-5'></div>
+                        <div className='w-[25%] flex flex-col  items-center justify-center '>
+                            <p>Earnings to data</p>
+                            <p className=' font-bold text-[2.1rem]'>$102,712.00</p>
+                            
+
+                        </div>
+                        <div className='h-[70%] w-[2px] bg-slate-200 mt-5'></div>
+                        <div className='w-[25%] flex flex-col  items-center justify-center '>
+                            <p>Total Reservation</p>
+                            <p className=' font-bold text-[2.1rem]'>$230.00</p>
+                            
+
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
             </div>
 
