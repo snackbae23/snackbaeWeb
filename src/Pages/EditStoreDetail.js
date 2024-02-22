12a53887f695b13f4e183a43237b51f4f983fb1e
Nitish0604 <iamnitishsah@gmail.com>
2024-02-23 00:54:11 +0530

    update

--- src/Pages/EditStoreDetail.js
+++ src/Pages/EditStoreDetail.js
@@ -89,51 +89,50 @@ const EditStoreDetail = () => {
 
     return (
         <div className='w-full h-fit flex flex-col'>
-            <div className="fixed mt-8  w-full h-[20px]  flex flex-row items-center justify-between  px-6 box-border  max-w-full  text-zinc-700 font-sans">
-                {/* <div className="absolute h-full w-full top-0 right-0 bottom-0 left-0 bg-white" /> */}
-                <div className="flex flex-row items-start left-1 py-0 px-6">
-                    <img
-                        className="relative w-28 h-16 object-cover z-20"
-                        loading="lazy"
-                        alt=""
-                        src={logo}
-                    />
-                </div>
-                <div className="flex flex-row items-start justify-start gap-4 max-w-full z-1">
-                    <img
-                        className="relative w-12 h-12 min-h-12"
-                        loading="lazy"
-                        alt=""
-                        src={group117}
-                    />
-                    <Link to="/profilesettings">
-                        <img
-                            className="relative w-12 h-12 min-h-12"
-                            loading="lazy"
-                            alt=""
-                            src={group752}
-                        /></Link>
-                    <Link to="/profile">
-                        <div className="flex flex-row items-center justify-start gap-2">
-                            <img
-                                className="relative rounded-2xl w-12 h-12 object-cover"
-                                loading="lazy"
-                                alt=""
-                                src={rect54}
-                            />
-                            <div className="flex flex-col items-start justify-start gap-0.5">
-                                <div className="relative tracking-[-0.02em] font-semibold whitespace-nowrap">
-                                    Adam Cooper
-                                </div>
-                                <div className="relative text-sm tracking-[-0.02em] whitespace-nowrap">
-                                    adamcooper@gmail.com
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-            </div>
-
+            <div className="z-10 bg-white fixed mt-8  w-full h-20 -top-8 flex flex-row items-center justify-between  px-6 box-border  max-w-full  text-zinc-700 font-sans">
+               
+               <div className="flex flex-row items-start left-1 py-0 px-6">
+                   <img
+                       className="relative w-28 h-16 object-cover z-20"
+                       loading="lazy"
+                       alt=""
+                       src={logo}
+                   />
+               </div>
+               <div className="flex flex-row items-start justify-start gap-4 max-w-full z-1">
+                   <img
+                       className="relative w-12 h-12 min-h-12"
+                       loading="lazy"
+                       alt=""
+                       src={group117}
+                   />
+                   <Link to="/profilesettings">
+                       <img
+                           className="relative w-12 h-12 min-h-12"
+                           loading="lazy"
+                           alt=""
+                           src={group752}
+                       /></Link>
+                   <Link to="/profile">
+                       <div className="flex flex-row items-center justify-start gap-2">
+                           <img
+                               className="relative rounded-2xl w-12 h-12 object-cover"
+                               loading="lazy"
+                               alt=""
+                               src={rect54}
+                           />
+                           <div className="flex flex-col items-start justify-start gap-0.5">
+                               <div className="relative tracking-[-0.02em] font-semibold whitespace-nowrap">
+                                   Adam Cooper
+                               </div>
+                               <div className="relative text-sm tracking-[-0.02em] whitespace-nowrap">
+                                   adamcooper@gmail.com
+                               </div>
+                           </div>
+                       </div>
+                   </Link>
+               </div>
+           </div>
             <div className='flex  w-full mt-20 h-fit'>
 
                 {/* left */}
